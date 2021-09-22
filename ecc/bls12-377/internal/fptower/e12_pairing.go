bed314f64b7fcc2ac2782aba2b071c1e2b8a9d5e
Thomas Piellard <thomas.piellard@consensys.net>
2021-09-22 17:49:12 +0200

    feat(bls12-377): addition of MulBy235

--- ecc/bls12-377/internal/fptower/e12_pairing.go
+++ ecc/bls12-377/internal/fptower/e12_pairing.go
@@ -16,12 +16,12 @@ func (z *E12) nSquareCompressed(n int) {
 func (z *E12) Expt(x *E12) *E12 {
 	// const tAbsVal uint64 = 9586122913090633729
 	// tAbsVal in binary: 1000010100001000110000000000000000000000000000000000000000000001
-	// drop the low 46 bits (all 0 except the least significant bit): 100001010000100011 = 136227
+	// drop the low 46 bits (all 0 except the least significant bit): 	 = 136227
 	// Shortest addition chains can be found at https://wwwhomes.uni-bielefeld.de/achim/addition_chain.html
 
 	var result, x33 E12
 
-	// a shortest addition chain for 136227
+	// a shortest addition chain for 136227 --> 17S, 4M instead of 18S, 5M
 	result.Set(x)
 	result.nSquare(5)
 	result.Mul(&result, x)
@@ -39,6 +39,58 @@ func (z *E12) Expt(x *E12) *E12 {
 	result.Mul(&result, x)
 
 	z.Set(&result)
+	return z
+}
+
+// 												  R1 R0   R2
+// MulBy235 multiplication by sparse element (0,0,c2,c3,0,c5)
+func (z *E12) MulBy245(c2, c3, c5 *E2) *E12 {
+
+	var t1, t2 E2
+	var res E12
+
+	t1.Mul(c3, &z.C1.B1)
+	t2.Mul(c2, &z.C0.B2)
+	res.C0.B0.Mul(c5, &z.C1.B0).
+		Add(&res.C0.B0, &t1).
+		Add(&res.C0.B0, &t2).
+		MulByNonResidue(&res.C0.B0)
+
+	t1.Mul(c3, &z.C1.B2)
+	t2.Mul(c5, &z.C1.B1)
+	t1.Add(&t1, &t2).MulByNonResidue(&t1)
+	res.C0.B1.Mul(c2, &z.C0.B0).
+		Add(&res.C0.B1, &t1)
+
+	t1.Mul(c3, &z.C1.B0)
+	t2.Mul(c2, &z.C0.B1)
+	res.C0.B2.Mul(c5, &z.C1.B2).
+		MulByNonResidue(&res.C0.B2).
+		Add(&res.C0.B2, &t1).
+		Add(&res.C0.B2, &t2)
+
+	t1.Mul(c3, &z.C0.B2)
+	t2.Mul(c2, &z.C1.B2)
+	res.C1.B0.Mul(c5, &z.C0.B1).
+		Add(&res.C1.B0, &t1).
+		Add(&res.C1.B0, &t2).
+		MulByNonResidue(&res.C1.B0)
+
+	t1.Mul(c3, &z.C0.B0)
+	t2.Mul(c2, &z.C1.B0)
+	res.C1.B1.Mul(c5, &z.C0.B2).
+		MulByNonResidue(&res.C1.B1).
+		Add(&res.C1.B1, &t1).
+		Add(&res.C1.B1, &t2)
+
+	t1.Mul(c3, &z.C0.B1)
+	t2.Mul(c2, &z.C1.B1)
+	res.C1.B2.Mul(c5, &z.C0.B0).
+		Add(&res.C1.B2, &t1).
+		Add(&res.C1.B2, &t2)
+
+	z.Set(&res)
+
 	return z
 }
 
